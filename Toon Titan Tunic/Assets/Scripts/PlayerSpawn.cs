0dc99078616cb8e970e081818b1b522c860ff8d8
Juan Fangano <105882155+JuanyFanga@users.noreply.github.com>
2024-08-22 21:36:19 -0300

    Rpc test

--- Toon Titan Tunic/Assets/Scripts/PlayerSpawn.cs
+++ Toon Titan Tunic/Assets/Scripts/PlayerSpawn.cs
@@ -1,15 +1,38 @@
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
 
 public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
     //hacer una lista con varias posiciones y poder elegir entre esas posiciones
+    
+    GameObject player;
+    private PhotonView _pv;
+
+    private void Awake()
+    {
+        _pv = GetComponent<PhotonView>();
+    }
 
     private void Start()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, 
-            new Vector3(Random.Range(-2, 2), 1, Random.Range(-2,2)), 
+        player = PhotonNetwork.Instantiate(playerPrefab.name,
+            new Vector3(Random.Range(-2, 2), 1, Random.Range(-2, 2)),
             Quaternion.identity);
     }
+
+    //[PunRPC]
+    //private void SetColorToPlayers()
+    //{
+    //    foreach (KeyValuePair<int, Photon.Realtime.Player> PlayersData in PhotonNetwork.CurrentRoom.Players)
+    //    {
+    //        switch (PlayersData.Value.UserId)
+    //        {
+                
+    //        }
+    //    }
+    //}
+
 }

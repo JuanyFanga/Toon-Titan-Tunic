1c1021bf3adcd5dfccd682b44d4bcaf795f2f354
Juan Fangano <105882155+JuanyFanga@users.noreply.github.com>
2024-09-05 20:01:10 -0300

    Missile more functionality

--- Toon Titan Tunic/Assets/Scripts/Weapon/Missile.cs
+++ Toon Titan Tunic/Assets/Scripts/Weapon/Missile.cs
@@ -6,6 +6,7 @@ public class Missile : MonoBehaviour
 {
     private bool _canExplode;
     [SerializeField] private float _speed;
+    [SerializeField] float bounceFactor = 0.08f;
 
     private void Awake()
     {
@@ -18,10 +19,38 @@ public class Missile : MonoBehaviour
         {
             transform.Translate(Vector3.forward * _speed * Time.deltaTime); ;
         }
+        else
+        {
+            transform.position = Vector3.zero;
+        }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
+        if (_canExplode && collision.collider.CompareTag("Player")) 
+        {
+            var hitPlayer = collision.gameObject.GetComponent<IPlayer>();
+            hitPlayer.Die();
+        }
 
+        if (_canExplode && !collision.collider.CompareTag("Player"))
+        {
+            Vector3 normal = collision.contacts[0].normal;
+
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 incomingVelocity = rb.velocity;
+            Vector3 reflectedVelocity = Vector3.Reflect(incomingVelocity, normal);
+
+            rb.velocity = reflectedVelocity * bounceFactor;
+        }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_canExplode && other.CompareTag("Player")) 
+        {
+            var hitPlayerWeapon = other.GetComponent<IWeapon>();
+            hitPlayerWeapon.Reload();
+        }
+    }
 }

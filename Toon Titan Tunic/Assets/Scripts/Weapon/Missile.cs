c775edf866f01fd594c20183694bef21ffc990e6
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2024-09-05 21:39:59 -0300

    elmi sil

--- Toon Titan Tunic/Assets/Scripts/Weapon/Missile.cs
+++ Toon Titan Tunic/Assets/Scripts/Weapon/Missile.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
@@ -7,51 +8,43 @@ public class Missile : MonoBehaviour
 {
     private bool _canExplode;
     [SerializeField] private float _speed;
-    [SerializeField] float bounceFactor = 0.08f;
-    private Rigidbody _rb;
-
+    [SerializeField] float bounceFactor = 1f;
+    private Vector3 _velocity;
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
-        _canExplode = true;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            _canExplode = true;
+            _velocity = Vector3.forward * _speed;
+        }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (_canExplode)
-        {
-            //transform.Translate(Vector3.forward * _speed * Time.deltaTime); ;
-            _rb.velocity = transform.forward * _speed;
+        {  
+            transform.position += _velocity * Time.fixedDeltaTime;
         }
-        else
-        {
-            transform.position = Vector3.zero;
-        }
-
-        Debug.Log($"Velocity is {_rb.velocity}");
-
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log($"Collision is {collision.gameObject.name}");
-
-        if (_canExplode && collision.collider.CompareTag("Player")) 
+        if (PhotonNetwork.IsMasterClient)
         {
-            var hitPlayer = collision.gameObject.GetComponent<IPlayer>();
-            hitPlayer.Die();
-        }
+            if (_canExplode && collision.collider.CompareTag("Player"))
+            {
+                var hitPlayer = collision.gameObject.GetComponent<IPlayer>();
+                hitPlayer.Die();
+            }
+            else if (_canExplode && !collision.collider.CompareTag("Player"))
+            {
+                Vector3 normal = collision.contacts[0].normal;
 
-        if (_canExplode && !collision.collider.CompareTag("Player"))
-        {
-            Vector3 normal = collision.contacts[0].normal;
-
-            Vector3 incomingVelocity = _rb.velocity;
-            Vector3 reflectedVelocity = Vector3.Reflect(incomingVelocity, normal);
-
-            _rb.velocity = reflectedVelocity * bounceFactor;
-
-            print("soy gaymer");
+                Vector3 reflectedVelocity = Vector3.Reflect(_velocity, normal);
+                _velocity.y = 0;
+                _velocity = reflectedVelocity;
+                transform.forward = _velocity.normalized;
+            }
         }
     }
 
